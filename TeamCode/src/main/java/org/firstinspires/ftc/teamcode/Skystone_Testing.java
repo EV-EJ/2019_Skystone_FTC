7333ca0ae5a2d38c3387eb8d4e4cdab76c20fffe
escape-velocity-14343 <mircosoft4hire@gmail.com>
2019-12-11 20:13:21 -0800

    Percy Jackson

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone_Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone_Testing.java
@@ -375,7 +375,7 @@ public class Skystone_Testing extends LinearOpMode  {
             //StrafeRightDistance(1,15);
             telemetry.addData("BEEEEP","EEEEEEEEEEEEEP");
             //  sleep(1000);
-            DriveBackward(0.02);
+            DriveBackward(0.08);
             boolean detected = false;
             //throw FileNotFoundException;
             //DriveBackwardDistance(1, 4);
